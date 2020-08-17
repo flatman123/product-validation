73e46e8845da1ae1b0f863217a7b12f80fddece8
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-08-17 09:43:50 -0400

    Added Button Component with Styles

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,35 +2,40 @@ import React from 'react'
 import classes from './Input.module.css';
 
 const input = props => {
-    let elements = props.elements.map((type,i) => {
-        if (type[1].elementConfig.type === 'text') {
-            let [elType,ph, vl] = [ 
-                        type[1].elementConfig.type,
-                        type[1].elementConfig.placeHolder,
-                        type[1].value
+    let [element,elementList] = [
+        null,
+        Object.entries(props.config),
+    ];
+    if (!props.elementsAreActive) {
+        element = elementList.map((el, index)=>{
+            let [elType, ph] = [
+                el[1].elementConfig.type,
+                el[1].elementConfig.placeHolder
             ];
-            
-            return (
-                <div key={vl + i} className={classes.InputDiv}>
-                    <label className={classes.Label}
-                            htmlFor={elType}>{ph}</label>
-                    <input
-                        className={classes.InputMargin}                     
-                        type={elType}                        
-                        placeholder={ph} 
-                        defaultValue={vl} 
-                        required
-                    />
-                </div>
-                )                                  
-        }})
-
-    return (    
-        <div className={classes.InputContainer}>
-         {elements}
-        </div>
-            
-       
+            switch(elType){
+                case 'text':
+                    return (
+                        <React.Fragment key={ph}>
+                            <label className={classes.TextLabel}
+                                    >{ph}</label>
+                            <input
+                                className={classes.InputMargin}
+                                onChange={props.inputChanged}
+                                placeholder={ph}
+                                required
+                            />
+                        </React.Fragment>
+                    );
+                break;
+                default:
+                    return null;
+            }                
+        });
+    };
+    return (
+            <div className={classes.InputDiv}>
+                {element}
+            </div>
     )
 };
 
