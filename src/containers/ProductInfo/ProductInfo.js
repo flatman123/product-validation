b3ab68fd8537a50844d54ae41b0a291929977811
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-08-17 09:48:31 -0400

    Initial commit for PRoductInfo component

---
+++ src/containers/ProductInfo/ProductInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from '../../Axios/baseAxios';
+
+class ProductInfo extends React.Component {
+    state = {
+
+    }
+    render(){
+        <div>PRODUCT INFO</div>
+    }
+};
+
+export default ProductInfo;
