67963dc4580feace2866961fd760dbc60863a8f6
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-08-17 09:40:23 -0400

    adjusted return statements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ProductForm from './containers/ProductForm/ProductForm';
 import classes from './App.module.css';
-
+import Header from './components/Header/Header';
 
 const App = () => {
-    return (
-        <div className={classes.App}>
-            <ProductForm />
-        </div>       
-    )
+        return (
+            <React.Fragment>
+                 <div className={classes.App}>
+                    <Header />
+                    <ProductForm />                    
+                </div>    
+            </React.Fragment>
+   
+        )
 };
 
 export default App;
