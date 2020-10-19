51db869f4b87cf75684b8cf5c8a76bbde662edd7
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-10-18 22:58:39 -0400

    Added URL For MongoDBAtlas database

--- src/Axios/baseAxios.js
+++ src/Axios/baseAxios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const db = 
 const instance = axios.create({
-    baseURL: 'https://proval-aa746.firebaseio.com/'
+    baseURL: 'https://proval-aa746.firebaseio.com/',
+    baseURL_2 : `mongodb+srv://redeyedb:cisco123@cluster0.yp66n.mongodb.net/product_info?retryWrites=true&w=majority`
 });
 
 export default instance;
